9103afbb5ea175172580aa8403d422b3f9408a3b
Filipp Makarov <filmakarov@Filipps-MacBook-Pro-2.local>
2024-03-20 14:10:27 +0000

    test base

---
+++ test/ERC7579PermissionValidator/ERC7579PV_Base.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "forge-std/Test.sol";
+
+
+contract TestBaseUtil is Test {
+
+    
+
+    function setUp() public virtual {
+
+    }
+}
